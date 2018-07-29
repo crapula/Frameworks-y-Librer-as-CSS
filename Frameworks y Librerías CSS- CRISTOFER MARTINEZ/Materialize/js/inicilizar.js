727687082560ee2207e2f519138ad8d16e84300d
crapula <36478557+crapula@users.noreply.github.com>
2018-07-29 16:37:32 -0500

    Add files via upload

---
+++ Frameworks y Librerías CSS- CRISTOFER MARTINEZ/Materialize/js/inicilizar.js
@@ -0,0 +1,12 @@
+
+M.AutoInit();
+ 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, 0);
+  });
+
+  
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
